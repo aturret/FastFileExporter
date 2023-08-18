1dc84aefff4d64c90eb85cd21cef1bfd762dfdde
aturret <enturreopy@gmail.com>
2023-08-17 22:56:45 -0400

    Add downloader route

--- app/main/download.py
+++ app/main/download.py
@@ -1,9 +1,75 @@
-from flask import current_app, request, jsonify
+import traceback
 
+from flask import current_app, request, jsonify
+from yt_dlp import YoutubeDL
 
 from app.main import main
 
 
+def init_yt_downloader(url,
+                       download=True,
+                       hd=False,
+                       audio_only=False,
+                       extractor=None) -> YoutubeDL:
+    config = current_app.config
+    if audio_only:
+        ydl_opts = {
+            'paths': {
+                'home': config['DOWNLOAD_DIR'],
+            },
+            'format': 'm4a/bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'm4a',
+            }]
+        }
+    else:
+        if extractor is None:
+            raise ValueError('extractor cannot be None')
+        elif extractor == 'youtube':
+            video_format = 'bestvideo[ext=mp4]+(258/256/140)/best' if hd \
+                else 'bv*[height<=480][ext=mp4]+ba[ext=m4a]/b[height<=480][ext=mp4]/ wv*+ba[ext=m4a]/w'
+        elif extractor == 'bilibili':
+            if hd and config['BILIBILI_COOKIE'] is not None:
+                cookies = config['BILIBILI_COOKIE']
+                video_format = 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'
+            else:
+                video_format = 'bv*[height<=480]+ba/b[height<=480] / wv*+ba/w'
+        ydl_opts = {
+            'paths': {
+                'home': config['DOWNLOAD_DIR'],
+            },
+            'outtmpl': {
+                'default': '%(title)s-%(id)s.%(ext)s',
+            },
+            'format': video_format,
+        }
+    downloader = YoutubeDL(ydl_opts)
+    return downloader
+
+
 @main.route('/download', methods=['POST'])
-def download():
-    url = request.get_json().get('url')
+def download_video():
+    try:
+        data = request.get_json()
+        url: str = data.get('url')
+        download: bool = data.get('download', True)
+        hd: bool = data.get('hd', False)
+        extractor: str = data.get('extractor')
+        audio_only: bool = data.get('audio_only', False)
+
+        downloader = init_yt_downloader(url=url, download=download, hd=hd, extractor=extractor, audio_only=audio_only)
+        with downloader:
+            downloader.download([url])
+            content_info = downloader.extract_info(url, download=False)
+            content_path = downloader.prepare_filename(content_info)
+        return jsonify({
+            'message': 'success',
+            'content_info': content_info,
+            'content_path': content_path,
+             }), 200
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify(
+            {'message': 'failed',
+             'error': str(e)}), 500
