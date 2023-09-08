7fe599299d4598018cec61c775c3da3e346c7e7e
aturret <enturreopy@gmail.com>
2023-09-07 21:33:05 -0400

    Update prompt

--- app/main/transcribe.py
+++ app/main/transcribe.py
@@ -46,13 +46,13 @@ def get_audio_text(audio_file: str, openai_api_key: str):
         audio_segment.export(audio_segment_path)
         print(f"audio_segment_path: {audio_segment_path}")
         audio_segment_file = open(audio_segment_path, "rb")
-        transcript_segment = openai.Audio.transcribe(TRANSCRIBE_MODEL,audio_segment_file)
+        transcript_segment = openai.Audio.transcribe(TRANSCRIBE_MODEL, audio_segment_file)
         transcript += str(transcript_segment["text"]).encode("utf-8").decode("utf-8")
         audio_segment_file.close()
         os.remove(audio_segment_path)
     os.remove(audio_file)
-    post_process_response = punctuation_assistant(transcript)
-    transcript = post_process_response['choices'][0]['message']['content']
+    transcript = punctuation_assistant(transcript)['choices'][0]['message']['content']
+    transcript = "全文总结：\n" + summary_assistant(transcript)['choices'][0]['message']['content'] + "\n原文：\n" + transcript
     print(f"transcript: {transcript}")
     return transcript
 
@@ -65,23 +65,46 @@ def milliseconds_until_sound(sound, silence_threshold_in_decibels=-20.0, chunk_s
     return trim_ms
 
 
-def punctuation_assistant(ascii_transcript):
+def punctuation_assistant(ascii_transcript: str):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-16k",
         temperature=0,
         messages=[
             {
                 "role": "system",
-                "content": "You are a helpful assistant that adds punctuation to text and make summary. "
-                           "You will give a text consists of two parts: summary and context.\n"
-                           "For the context part, Preserve the original words and only insert necessary punctuation "
-                           "such as periods, commas, capialization, symbols like dollar signs or percentage signs, and "
-                           "formatting according to the language of the provided context. And I hope you to separate "
-                           "the context into several paragraphs based on the meaning.\nFor the summary part, the format"
-                           "you should make a brief summary of the whole context and put it at the beginning of the "
-                           "response. The format of the summary is like \"\"\"Summary: xxx\"\"\". \n"
-                           "Please use only the context provided. \n"
-                           "If there is no context provided say, 'No context provided'\n"
+                "content": "You are a helpful assistant. Your job is to adds punctuation to text. "
+                           "What you are going to do should follow the rules below: \n"
+                           "1. You have received a text which is transcribed from an audio file which we call it \"original text\".\n"
+                           "2. The response should be presented in the language of the original text.\n"
+                           "3. I need you to convert the original text into a new context. During "
+                           "this process, please preserve the original words of the given original text and only insert "
+                           "necessary punctuation such as periods, commas, capialization, symbols like dollar signs or "
+                           "percentage signs, and formatting according to the language of the provided original text. And I "
+                           "hope you to separate the original text into several paragraphs based on the meaning. Please "
+                           "use only the provided original text. \n"
+            },
+            {
+                "role": "user",
+                "content": ascii_transcript
+            }
+        ]
+    )
+    return response
+
+
+def summary_assistant(ascii_transcript: str):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        temperature=0,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant. Your job is to summarize text. "
+                           "What you are going to do should follow the rules below: \n"
+                           "1. You have received a text which we call it \"original text\".\n"
+                           "2. The response should be presented in the language of the original text.\n"
+                           "3. I need you to make a brief statement of the main points of the original text."
+                           "Please use only the provided original text. \n"
             },
             {
                 "role": "user",
