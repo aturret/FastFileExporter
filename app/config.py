36bd362c89301715a01a00304d362628b58cc3af
aturret <enturreopy@gmail.com>
2023-09-10 15:59:48 -0400

    Rename files

--- app/config.py
+++ app/config.py
@@ -9,7 +9,6 @@ class Config:
     @staticmethod
     def init_app(app):
         pass
-
     pass
 
 
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
-from . import download, pdf_export, transcribe
+from . import video_download, pdf_export, transcribe
