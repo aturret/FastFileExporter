30625fe1dea0d4d2fc286b61543b0a481263bb25
aturret <enturreopy@gmail.com>
2024-11-21 16:53:17 -0500

    Update weasyprint version

--- app/config.py
+++ app/config.py
@@ -1,13 +1,36 @@
 import os
 import tempfile
+from typing import Optional
 
 env = os.environ
 
 
+def get_bool(value: Optional[str], default: bool = True) -> bool:
+    true_values = ("True", "true", "1", "yes", "on")
+    false_values = ("False", "false", "0", "no", "off")
+
+    if value is None:
+        return default
+    value = value.lower()
+
+    if value in true_values:
+        return True
+    elif value in false_values:
+        return False
+    else:
+        return default
+
+
+def get_env_bool(env, var_name: Optional[str], default: bool = False) -> bool:
+    """Retrieve environment variable as a boolean."""
+    value = env.get(var_name, "").lower()
+    return get_bool(value, default)
+
+
 class Config:
-    LOCAL_MODE = env.get("LOCAL_MODE", True)
+    LOCAL_MODE = get_bool(env.get("LOCAL_MODE", 'false'), False)
     BASE_URL = env.get("BASE_URL", "localhost:4000")
-    PROXY_MODE = env.get("PROXY_MODE", False).lower() in ['true', '1', 't', 'y', 'yes']
+    PROXY_MODE = get_bool(env.get("PROXY_MODE", 'false'), False)
     PROXY_URL = env.get("PROXY_URL", "http://localhost:4000")
 
     @staticmethod
